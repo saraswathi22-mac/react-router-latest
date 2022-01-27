3b279e4e629def27e1210a6b8050b4c4e4dcabbd
exit <saraswathi.mac@gmail.com>
2022-01-27 17:55:49 +0530

    Links

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react-router-latest
+
+npm i react-router-dom
+
+1. Configuring Routes : BrowserRouter, Routes, Route
+
+2. Links
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
